4a4466cc97fb5fef885b48adf6bb93db07f9ab73
jsg5383 <jsg5383@gmail.com>
2020-06-12 19:52:10 +0900

    commit

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -2,12 +2,35 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+const config = require('./config/key');
 
-mongoose.connect('mongodb+srv://seolgi:JHAHds20LIix0uop@boiler-plate-hwpfc.mongodb.net/<dbname>?retryWrites=true&w=majority',{
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+//application/json
+app.use(bodyParser.json());
+
+mongoose.connect(config.mongoURI,{
     useNewUrlParser:true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify:false
 }).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!aaaaaaa'))
+
+
+app.post('/register', (req, res) =>{
+
+    const user = new User(req.body);
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({success:false, err})
+        return res.status(200).json({
+            success:true
+        })
+    })
+
+
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
--- boiler-plate/models/User.js
+++ boiler-plate/models/User.js
@@ -31,5 +31,7 @@ const userSchema = mongoose.Schema({
     }
 })
 
-const user = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports = { User };
 
